1b41d3371c52354a4ce6431ece048c8b1a0e95cb
rahul <rkrahul8181@gmail.com>
2024-08-20 11:10:54 +0530

    feat: Add routes for public, authentication, and API endpoints

---
+++ routes.ts
@@ -0,0 +1,30 @@
+
+/**
+ * Represents the public routes.
+ * @type {string[]}
+ */
+export const publicRoutes = [
+    "/"
+]
+
+/**
+ * Represents the authentication routes.
+ * @type {string[]}
+ */
+export const authRoutes = [
+    "/auth/login",
+    "/auth/signup",
+]
+
+
+/**
+ * Represents the api prefix.
+ * @type {string[]}
+ */
+export const apiRoutesPrefix = "/api"
+
+/**
+ * entry point for if user is logedIn
+ * @type {string}
+ */
+export const DEFAULT_LOGEDIN_URL = "/settings"
