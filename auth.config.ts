2125bd2cd37138f72f300bc959503543f3064c21
rahul <rkrahul8181@gmail.com>
2024-08-20 11:11:19 +0530

    add authconifg for working with prisma and middleware

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,29 @@
-import GitHub from "next-auth/providers/github"
-import type { NextAuthConfig } from "next-auth"
- 
+import Credentials from "next-auth/providers/credentials";
+import type { NextAuthConfig } from "next-auth";
+import bcrypt from "bcryptjs";
+import { loginSchema } from "@/schemas/index";
+import { getUserByEmail } from "@/data/user";
+
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub],
-} satisfies NextAuthConfig
+  providers: [
+    Credentials({
+      async authorize(credential) {
+        const validationResult = loginSchema.safeParse(credential);
+        if (validationResult.success) {
+          const { email, password } = validationResult.data;
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) {
+            return null;
+          }
+          const matchedPassword = await bcrypt.compare(password, user.password);
+          if (!matchedPassword) {
+            return null;
+          }
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
