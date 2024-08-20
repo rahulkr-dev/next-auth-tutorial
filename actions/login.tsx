acf390bdd44a227663135a4265abd61ad089b28a
rahul <rkrahul8181@gmail.com>
2024-08-20 11:12:40 +0530

    add login funcalities in action

--- actions/login.tsx
+++ actions/login.tsx
@@ -1,16 +1,28 @@
-"use server"
-import * as z from "zod"
-import {loginSchema} from "@/schemas"
+"use server";
+import * as z from "zod";
+import { loginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { DEFAULT_LOGEDIN_URL } from "@/routes";
 
-export const login = async (values:z.infer<typeof loginSchema>)=>{
-    const validationResult = loginSchema.safeParse(values)
+export const login = async (values: z.infer<typeof loginSchema>) => {
+  const validationResult = loginSchema.safeParse(values);
 
- 
-    if(!validationResult.success){
-        return {error:"Something went wrong"}
+  if (!validationResult.success) {
+    return { error: "Something went wrong" };
+  }
+  const { email, password } = validationResult.data;
+  try {
+    await signIn("credentials", { email, password, redirectTo: DEFAULT_LOGEDIN_URL });
+  } catch (err) {
+    if (err instanceof AuthError) {
+      switch (err.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid Credentials" };
+        default:
+          return { error: "Something went wrong" };
+      }
     }
-
-    return {
-        message:"Email Sent"
-    }
-}
+    throw err;
+  }
+};
